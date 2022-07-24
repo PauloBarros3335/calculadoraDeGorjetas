4d2ef8ebef172c9eb3a65903641c0a3ae5e48150
unknown <barrosp335@gmail.com>
2022-07-24 17:11:53 -0300

    alterado texto e imagem

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
+
+
 function calculateTip(event){
-event.preventDefault();
-
-let bill = document.getElementById("bill").Value;
-let servicequal = document.getElementById("servicequal").Value;
-let numOfpeople = document.getElementById("people").Value;
-
-
-if (bill == 0 | servicequal == 0 ) {
-   alert("por vafor preencha os valores");
-    return;
-}
-if(numOfpeople == "" | numOfpeople <= 1 ){
-numOfpeople = 1;
-document.getElementById('each').style.display ="none";
-}else{
-   document.getElementById('each').style.display ="block"; 
-
-}
-let total = (bill * servicequal) / numOfpeople;
-total = total.toFixed(2)
-document.getElementById('tip').innerHTML = total;
-document.getElementById('totaltip').style.display ="block";
-
-}
-document.getElementById('totaltip').style.display ="none";
-document.getElementById('each').style.display ="none";
-document.getElementById('tipsForm').addEventListener('submit'  ,calculateTip );
+  event.preventDefault();
+  
+  let bill = document.getElementById("bill");
+  let servicequal = document.getElementById("servicequal");
+  let numOfpeople = document.getElementById("people");
+  
+  
+  if (bill.value == 0 || servicequal.value == 0 ) {
+     alert("por vafor preencha os valores");
+      return;
+  }
+  if(numOfpeople.value == "" || numOfpeople.value <= 1 ){
+  numOfpeople.value = 1;
+  document.getElementById('each').style.display ="none";
+  }else{
+     document.getElementById('each').style.display ="block"; 
+  
+  }
+  let total = (bill.value * servicequal.value) / numOfpeople.value;
+  total = total.toFixed(2)
+  document.getElementById('tip').innerHTML = total;
+  document.getElementById('totaltip').style.display ="block";
+  
+  }
+  document.getElementById('totaltip').style.display ="none";
+  document.getElementById('each').style.display ="none";
+  document.getElementById('tipsForm').addEventListener('submit'  ,calculateTip );
+   
